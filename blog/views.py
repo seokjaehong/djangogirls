1f67226ae2831e4c6f87ff625cea9f298ad43a8f
seokjae hong <hsj2334@algorithmlabs.co.kr>
2020-06-15 21:40:17 +0900

    publish 기능 add

--- blog/views.py
+++ blog/views.py
@@ -8,13 +8,20 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    # posts= Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    posts = Post.objects.all()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by("created_date")
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
@@ -24,12 +31,12 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_new.html', {'form': form})
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 
 def post_edit(request, pk):
@@ -39,9 +46,15 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
